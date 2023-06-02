94f4debb21414432f00f2dc7b17670bbc602ff7b
Sergey Kalabin <Bams-5@mail.ru>
2023-06-02 21:48:35 +0400

    выполнил 2 задачки: по усечению строки и по Заглавной букве

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,5 +1,8 @@
 function ucFirst(str) {
   // ваш код...
   const strTrim = str.trim();
+  if (str.length === 0 && str === '') {
+    return '';
+  };
   return `${strTrim[0].toUpperCase()}${strTrim.slice(1)}`;
-}
+};
