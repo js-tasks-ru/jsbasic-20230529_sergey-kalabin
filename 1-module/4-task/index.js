6f368d64efebac8f7bd3579039e6ddfe730d6218
Sergey Kalabin <Bams-5@mail.ru>
2023-06-05 18:10:40 +0400

    решил 2 и 4 задачки 1 модуля

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
-}
+  let strUpper = str.toUpperCase();
+  if (strUpper.includes('1XBET')) {
+    return true;
+  }
+  else if (strUpper.includes('XXX')) {
+    return true;
+  }
+  return false;
+};
